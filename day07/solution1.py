92f2d6be32356ab18893dc2d402846b15815297f
alirezaghey <alireza.q1357@gmail.com>
2021-12-07 06:32:19 +0100

    add day 7 of advent of code solution

---
+++ day07/solution1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+from collections import defaultdict
+
+if len(sys.argv) != 2:
+    print("Usage: ./solution1.py <input_file>")
+    sys.exit(1)
+if not os.path.isfile(sys.argv[1]):
+    print("Error: no such file:", sys.argv[1])
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+with open(filename, "r") as f:
+    positions = list(map(int, f.readline().split(",")))
+
+m = max(positions)
+best = float("inf")
+coord = 0
+
+for i in range(0, m+1):
+    res = 0
+    for pos in positions:
+        res += abs(pos - i)
+    if res < best:
+        best = res
+        coord = i
+    else:
+        break
+print(f'cost: {best}, coord: {coord}')
